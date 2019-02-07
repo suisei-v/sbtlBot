4018e78b77761f2d9dbd25ed5849dabd08e8c885
suisei-v <suisei.v@yandex.ru>
2019-02-07 22:35:33 +0000

    fix

--- TelegramBot.php
+++ TelegramBot.php
@@ -2,17 +2,17 @@
 
 class TelegramBot
 {
-	protected $apiUrl;
-	protected $proxy;
+    protected $apiUrl;
+    protected $proxy;
     protected $timeout = 10;
     protected $connect_timeout = 5;
     protected $mysqli;
-	
-	public function __construct($token, $proxy = array())
-	{
-		$this->apiUrl = 'https://api.telegram.org/bot' . $token . '/';
+    
+    public function __construct($token, $proxy = array())
+    {
+        $this->apiUrl = 'https://api.telegram.org/bot' . $token . '/';
         $this->proxy = $proxy;
-	}
+    }
 
     public function initdb($db)
     {
@@ -38,15 +38,15 @@ class TelegramBot
         return $res;
     }
     
-	public function request($method, $params = array())
-	{
-		$params_str = http_build_query($params);
-		$url = $this->apiUrl . $method;
-		if ($params_str)
-			$url .= '?' . $params_str;
-		$ch = curl_init($url);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
+    public function request($method, $params = array())
+    {
+        $params_str = http_build_query($params);
+        $url = $this->apiUrl . $method;
+        if ($params_str)
+            $url .= '?' . $params_str;
+        $ch = curl_init($url);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->connect_timeout);
@@ -55,15 +55,15 @@ class TelegramBot
             curl_setopt($ch, CURLOPT_PROXYTYPE, $this->proxy['type']);
             curl_setopt($ch, CURLOPT_PROXYUSERPWD, $this->proxy['auth']);
         }
-		$res = curl_exec($ch);
+        $res = curl_exec($ch);
         $error = curl_error($ch);
         if ($error)
             error_log("curl error:  " . $error);
-		curl_close($ch);
+        curl_close($ch);
         if (!$error) {
             $res = json_decode($res, true);
             if (!$res['ok'])
                 error_log("$method error:  " . $res['description']);
         }
-	}
+    }
 }
